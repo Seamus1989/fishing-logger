990fed9a7f67b2bded7f33dc0949151e9905515b
Seamus1989 <seamus.reeve@gmail.com>
2021-04-12 11:39:30 +0100

    fix: new updated user modal to display all fish

--- src/components/modals/user-modal.tsx
+++ src/components/modals/user-modal.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import {useUserContext} from '../../context/all-user-score';
-import {roundToDecimanPlace} from '../../utils';
-import {AllLoggedFish} from '../all-logged-fish';
+import {AllLoggedFish} from '../molecules/all-logged-fish';
+import {TableHeader} from '../common/table-header';
 import {Box} from '../common/box';
-import {Text} from '../common/text';
 import {Modal} from './modal-components/modal';
+import {TextDisplayColumn} from '../common/text-display-column';
+import {Text} from '../common/text';
+import {roundToDecimanPlace} from '../../utils';
 
 const StyledContainer = styled.div`
   overflow-y: scroll;
@@ -17,50 +19,30 @@ export const UserModalContent = () => {
     ? users.find((thisUser) => thisUser.name === currentUser)
     : null;
 
-  const bonusPoints = user?.bonusPoints || 0;
-
   if (!user) return null;
   return (
     <Modal title="User Score">
       <StyledContainer>
         <Box p="20px" flex={1} bg="#FFC2BB">
-          <Box m="20px">
+          <Box pl="10px" pb="25px">
             <Box pb="5px">
-              <Text lineHeight="24px" fontWeight={600} fontSize="21px">
+              <Text lineHeight="18px" fontWeight={600} fontSize="16px">
                 {user ? user.name : ''}
               </Text>
             </Box>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Points: {user ? roundToDecimanPlace(user.score) : 0}
-            </Text>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Fish: {user?.allFish?.length || 0}
-            </Text>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Specimen:{' '}
-            </Text>
+            <TextDisplayColumn
+              text={`Total Fish: ${user?.allFish?.length || 0}`}
+            />
+            <TextDisplayColumn
+              text={`Total Specimen: ${user?.totalSpecimenNumber || 0}`}
+            />
+            <TextDisplayColumn
+              text={`Score Points: ${
+                user ? roundToDecimanPlace(user.score) : 0
+              }`}
+            />
           </Box>
-
-          <Box>
-            <Box pb="5px">
-              <Text lineHeight="20px" fontWeight={600} fontSize="18px">
-                {user ? user.name : ''}
-              </Text>
-            </Box>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Fish: {user?.allFish?.length || 0}
-            </Text>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Specimen: {user?.specimenStringArray.join(' ') || 0}
-            </Text>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Bonus Points: {roundToDecimanPlace(bonusPoints)}
-            </Text>
-            <Text lineHeight="18px" fontWeight={300} fontSize="14px">
-              Total Points:{' '}
-              {user ? roundToDecimanPlace(user.score + bonusPoints) : 0}
-            </Text>
-          </Box>
+          <TableHeader text="All Logged Fish" />
           <AllLoggedFish user={user} />
         </Box>
       </StyledContainer>
