4f08135fd04825ba11c63f2644a48aa271e9dec5
Seamus1989 <seamus.reeve@gmail.com>
2021-04-12 11:38:15 +0100

    fix: filter fish fix

--- src/context/fish-list.tsx
+++ src/context/fish-list.tsx
@@ -8,7 +8,7 @@ const FishContext = createContext<{
   fish: FishList | null;
   region: Region | null;
   filterFish: (name: string) => void;
-  toggleRegion: (selection: Region) => void;
+  toggleRegion: (selection?: Region) => void;
 }>({
   fish: null,
   region: null,
@@ -19,18 +19,38 @@ const FishContext = createContext<{
 const FishProvider = (props: {children: JSX.Element}): JSX.Element => {
   const [fish, setFish] = useState<null | FishList>(null);
   const [region, setRegion] = useState<null | Region>(null);
+
   const filterFish = useCallback(
     (value: string) => {
-      if (!fish) return;
-      const result = fish.filter((singleFish) => {
+      const indexOfRegionSelected = allFishedWithRegions.findIndex(
+        (e) => e.region === region,
+      );
+      if (value === '' && region) {
+        if (indexOfRegionSelected === -1) {
+          // throw error ???
+          return;
+        }
+        setFish(allFishedWithRegions[indexOfRegionSelected].fish);
+        return;
+      }
+      if (indexOfRegionSelected === -1) {
+        // throw error ???
+        return;
+      }
+      const currentFish = allFishedWithRegions[indexOfRegionSelected].fish;
+      const result = currentFish.filter((singleFish) => {
         return singleFish.name.toLowerCase().includes(value.toLowerCase());
       });
       setFish(result);
     },
-    [fish],
+    [region],
   );
 
-  const toggleRegion = useCallback((selection: Region) => {
+  const toggleRegion = useCallback((selection?: Region) => {
+    if (!selection) {
+      setRegion(null);
+      setFish(null);
+    }
     const indexOfSelection = allFishedWithRegions.findIndex(
       (e) => e.region === selection,
     );
