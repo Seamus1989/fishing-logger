5f8023daae7fcd1e3ba0157507d737966e894f3e
Seamus1989 <seamus.reeve@gmail.com>
2021-05-07 22:08:53 +0100

    Revert "fix: some clean up"
    This reverts commit 1252f13b155ecf44d52c5025b457fe9094d2399c.

--- src/components/molecules/single-logged-fish.tsx
+++ src/components/molecules/single-logged-fish.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {randomFishEmojiGenerator} from '../../consts';
 import {useUserContext} from '../../context/all-user-score';
 import {roundToDecimalPlace} from '../../utils';
@@ -26,54 +26,6 @@ export const SingleLoggedFish = ({
 }) => {
   const {deleteFish} = useUserContext();
   const isSpecimen = recordedWeight >= specimenWeight;
-
-  const binComponent = useMemo(() => {
-    return (
-      <Box
-        flex={0.5}
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-      >
-        <Box justifyContent="center" display="flex" flexDirection="column">
-          <Box
-            mb="3px"
-            borderBottom="1px rgba(0,0,0,0.4) solid"
-            onClick={() => deleteFish(id)}
-          >
-            <StyledImage height={15} width={15} src={bin} alt="logo" />
-          </Box>
-        </Box>
-      </Box>
-    );
-  }, [deleteFish, id]);
-
-  const fishDataRow = useMemo(() => {
-    return (
-      <>
-        <TextDisplayRow
-          verticalPadding={5}
-          leftPadding={3}
-          text={
-            !isSpecimen
-              ? name
-              : `${name} ${randomFishEmojiGenerator(Math.random())}`
-          }
-        />
-        <TextDisplayRow
-          verticalPadding={5}
-          leftPadding={5}
-          text={`${roundToDecimalPlace(scoredPoints)}`}
-        />
-        <TextDisplayRow verticalPadding={5} text={`${region}`} />
-        <TextDisplayRow
-          verticalPadding={5}
-          leftPadding={10}
-          text={`${roundToDecimalPlace(recordedWeight)} lbs`}
-        />
-      </>
-    );
-  }, [isSpecimen, name, recordedWeight, region, scoredPoints]);
   if (!isFirst) {
     return (
       <Box display="flex" pb="3px" flexDirection="row">
@@ -94,10 +46,24 @@ export const SingleLoggedFish = ({
         <TextDisplayRow verticalPadding={5} text={`${region}`} />
         <TextDisplayRow
           verticalPadding={5}
-          leftPadding={10}
           text={`${roundToDecimalPlace(recordedWeight)} lbs`}
         />
-        {binComponent}
+        <Box
+          flex={1}
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+        >
+          <Box justifyContent="center" display="flex" flexDirection="column">
+            <Box
+              mb="3px"
+              borderBottom="1px rgba(0,0,0,0.4) solid"
+              onClick={() => deleteFish(id)}
+            >
+              <StyledImage height={15} width={15} src={bin} alt="logo" />
+            </Box>
+          </Box>
+        </Box>
       </Box>
     );
   }
@@ -114,14 +80,47 @@ export const SingleLoggedFish = ({
           <TextDisplayRow leftPadding={3} bold text="Fish" />
           <TextDisplayRow leftPadding={5} bold text="Score" />
           <TextDisplayRow bold text="Region" />
-          <TextDisplayRow leftPadding={10} bold text="Weight" />
-          <Box flex={0.5} display="flex" flexDirection="column" />
+          <TextDisplayRow bold text="Weight" />
+          <Box flex={1} display="flex" flexDirection="column" />
         </Box>
 
         <Box display="flex" pb="3px" flexDirection="row">
-          {fishDataRow}
+          <TextDisplayRow
+            verticalPadding={5}
+            leftPadding={3}
+            text={
+              !isSpecimen
+                ? name
+                : `${name} ${randomFishEmojiGenerator(Math.random())}`
+            }
+          />
+          <TextDisplayRow
+            verticalPadding={5}
+            leftPadding={5}
+            text={`${roundToDecimalPlace(scoredPoints)}`}
+          />
+          <TextDisplayRow verticalPadding={5} text={`${region}`} />
+          <TextDisplayRow
+            verticalPadding={5}
+            text={`${roundToDecimalPlace(recordedWeight)} lbs`}
+          />
 
-          {binComponent}
+          <Box
+            flex={1}
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+          >
+            <Box justifyContent="center" display="flex" flexDirection="column">
+              <Box
+                mb="3px"
+                borderBottom="1px rgba(0,0,0,0.4) solid"
+                onClick={() => deleteFish(id)}
+              >
+                <StyledImage height={15} width={15} src={bin} alt="logo" />
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </>
